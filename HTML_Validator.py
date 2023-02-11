9ea225151469c516adfab69bd1c8c2158f3e3dad
ains-arch <ainsleearchibald@gmail.com>
2023-02-11 13:09:04 -0800

    modified HTML Validator to pass main tests

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -17,6 +17,37 @@ def validate_html(html):
     # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
+    stack = []
+    text = _extract_tags(html)
+    paren_text = \
+        ''.join([html[i] for i in range(len(html)) if (html[i] in "<>")])
+    print("paren_text=", paren_text)
+    for i, symbol in enumerate(paren_text):
+        if symbol == '<':
+            stack.append(symbol)
+        else:
+            if len(stack) == 0:
+                return False
+            stack.pop()
+    if len(stack) == 0:
+        pass
+    else:
+        return False
+
+    for symbol in text:
+        if symbol[1] != '/':
+            stack.append(symbol)
+        else:
+            if len(stack) == 0:
+                return False
+            if symbol[1] == '/':
+                if stack[-1] == \
+                        ''.join([symbol[i] for i in range(len(symbol)) if i != 1]):
+                    stack.pop()
+            else:
+                return False
+    return len(stack) == 0
+
 
 def _extract_tags(html):
     '''
@@ -29,3 +60,12 @@ def _extract_tags(html):
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+
+    tags = []
+    for i in range(len(html)):
+        if html[i] == '<':
+            for j in range(len(html[i:])):
+                if html[i+j] == '>':
+                    tags.append(html[i:i+j+1])
+                    break
+    return tags
